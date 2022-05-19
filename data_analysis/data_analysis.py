048e0fa7615876682d5d34177bd1ab332987eecb
meresophisticated <mai.praskalo@gmail.com>
2022-05-19 14:30:29 +0200

    Added docstrings Plots are now saved to .png files

--- data_analysis/data_analysis.py
+++ data_analysis/data_analysis.py
@@ -11,11 +11,11 @@ from data_retrieval import get_wind_forecast, \
     get_transformed_day_ahead, get_intra_day_min_max_mean, get_diff, get_pct_change_dataframe, get_intra_day_by_hours
 
 
-# Might make sense to do an analysis how price variance changes depending on how much time is left till closing time
-# TODO: save plots in files / labeli s slovenščini
+path = "../data_analysis/plots"
 
 
-def plot_price_at_given_time(start_date='2022-02-03', end_date='2022-03-03'):
+def plot_price_at_given_time(start_date='2021-11-09',
+                             end_date='2022-03-23'):
     """
     Plots intra-day average, min/max area and day-ahead price
     :param start_date: start date of plot (x-axis)
@@ -38,13 +38,36 @@ def plot_price_at_given_time(start_date='2022-02-03', end_date='2022-03-03'):
     ax.set_ylabel("Price")
     plt.tight_layout()
     plt.figure(figsize=(200, 150))
-    plt.show()
+    plt.savefig(f'{path}/price_at_given_time_{start_date}-{end_date}.png')
 
 
-def plot_average_price_of_product(start_date='2022-02-03', end_date='2022-02-03', time_before_closing=None, unit=None):
+def plot_average_price_of_product(max_time_before_closing=None,
+                                  min_time_before_closing=None,
+                                  unit=None,
+                                  start_date='2021-11-09',
+                                  end_date='2022-03-23'):
+    """
+    Plots the average price of product
+    :param max_time_before_closing: only trades after maximum till product closes
+    :param min_time_before_closing: only trades before minimum time till product closes
+    :param unit: pandas unit for time before closing
+        Possible values:
+        ‘W’, ‘D’, ‘T’, ‘S’, ‘L’, ‘U’, or ‘N’
+        ‘days’ or ‘day’
+        ‘hours’, ‘hour’, ‘hr’, or ‘h’
+        ‘minutes’, ‘minute’, ‘min’, or ‘m’
+        ‘seconds’, ‘second’, or ‘sec’
+        ‘milliseconds’, ‘millisecond’, ‘millis’, or ‘milli’
+        ‘microseconds’, ‘microsecond’, ‘micros’, or ‘micro’
+        ‘nanoseconds’, ‘nanosecond’, ‘nanos’, ‘nano’, or ‘ns’.
+    :param start_date: filter trades to those that happened on or after start_date
+    :param end_date: filter trades to those that happened on or before end_date
+    :return: None
+    """
     df_intra_day = get_intra_day_min_max_mean(interval='1H',
                                               on='trd_delivery_time_start',
-                                              time_before_closing=time_before_closing,
+                                              max_time_before_closing=max_time_before_closing,
+                                              min_time_before_closing=min_time_before_closing,
                                               unit=unit,
                                               start_date=start_date,
                                               end_date=end_date)
@@ -59,25 +82,38 @@ def plot_average_price_of_product(start_date='2022-02-03', end_date='2022-02-03'
     df_day_ahead.plot(x='trd_delivery_time_start', y='trd_price', use_index=True, ax=ax)
 
     ax.legend(["Intra-day average", "Min/Max Intra-day", "Day-ahead"])
-    if time_before_closing:
-        ax.set_title(f"Average price of product {time_before_closing} {unit} before closing")
+    if max_time_before_closing:
+        ax.set_title(f"Average price of product {max_time_before_closing} {unit} before closing")
     else:
         ax.set_title("Average price of product")
     ax.set_xlabel("Product start time")
     ax.set_ylabel("Price")
     plt.tight_layout()
     plt.figure(figsize=(200, 150))
-    plt.show()
+    plt.savefig(f'{path}/average_price_{start_date}-{end_date}'
+                f'-m{min_time_before_closing}-M{min_time_before_closing}-{unit}.png')
 
 
-def day_ahead_as_intra_day_prediction_accuracy(start_date='2022-02-02', end_date='2022-03-23', box_plot=False):
+def get_day_ahead_as_intra_day_prediction_accuracy(box_plot=False,
+                                                   start_date='2021-11-09',
+                                                   end_date='2022-03-23'):
+    """
+    Calculates standard deviation for intra-day prices by day
+    TODO: add option to input max/min time before closing to get_pct_change, maybe even add param to this function to group by something other than hour / not group
+    TODO: add option to not remove outliers
+    TODO: maybe change to actual price diff?
+    :param box_plot: to plot the box plot or not
+    :param start_date: filter trades to those that happened on or after start_date
+    :param end_date: filter trades to those that happened on or before end_date
+    :return:
+    """
     df = get_pct_change_dataframe(start_date, end_date)
 
     if box_plot:
         # Quantile box plot
         plt.boxplot(df['percentage_change'])
         plt.title("Before removing outliers")
-        plt.show()
+        plt.savefig(f'{path}/box_plot_before-{start_date}-{end_date}.png')
 
     # Calculate limits for  outliers with interquartile range
     # Will be more accurate when we get more data
@@ -95,25 +131,34 @@ def day_ahead_as_intra_day_prediction_accuracy(start_date='2022-02-02', end_date
         # Quantile box plot
         plt.boxplot(df['percentage_change'])
         plt.title("After removing outliers")
-        plt.show()
+        plt.savefig(f'{path}/box_plot_after-{start_date}-{end_date}.png')
 
     df.plot(kind='hist', bins=100, color='blue', edgecolor='black')
-    plt.show()
+    plt.savefig(f'{path}/percentage_change_hist.png')
 
     sns.distplot(df['percentage_change'], hist=True, kde=True,
                  bins=100, color='darkblue',
                  hist_kws={'edgecolor': 'black'},
                  kde_kws={'linewidth': 2})
-    plt.show()
+    plt.savefig(f'{path}/percentage_change_dist.png')
 
     # var and std by hour of day
     times = pd.to_datetime(df['trd_delivery_time_start'])
+    # TODO: make this return instead
     print(df.groupby(times.dt.hour)['percentage_change'].agg(['var', 'std']))
 
 
-def increase_decrease_analysis(start_date='2022-02-02', end_date='2022-03-23'):
+def get_increase_decrease(start_date='2021-11-09',
+                          end_date='2022-03-23'):
+    """
+    Calculates times when price decreased, increased or remained the same
+    TODO: create a separate function for value counts, this one should just return labeled data
+    :param start_date: filter trades to those that happened on or after start_date
+    :param end_date: filter trades to those that happened on or before end_date
+    :return: pandas dataframe with value counts for positive, negative and neutral changes
+    """
     df_intra_day = get_intra_day_min_max_mean(interval='H', on='trd_delivery_time_start', start_date=start_date,
-                                              end_date=end_date, time_before_closing=1, unit='H')
+                                              end_date=end_date, max_time_before_closing=1, unit='H')
 
     df = get_transformed_day_ahead(start_date=start_date, end_date=end_date)
 
@@ -129,10 +174,20 @@ def increase_decrease_analysis(start_date='2022-02-02', end_date='2022-03-23'):
         'Negative'
     ]
     df['label'] = np.select(conditions, values, default='Undefined')
+    # TODO: make this return instead
     print(df['label'].value_counts())
 
 
-def wind_analysis(time, start_date='2022-02-02', end_date='2022-03-23'):
+def get_wind_correlation(time,
+                         start_date='2021-11-09',
+                         end_date='2022-03-23'):
+    """
+    Calculates correlation between wind forecast for each region and change in price
+    :param time: time at which forecast happened
+    :param start_date: filter trades to those that happened on or after start_date
+    :param end_date: filter trades to those that happened on or before end_date
+    :return: dataframe of co
+    """
     df_wind = get_wind_forecast()
     df_wind = df_wind[df_wind['Time'] == time]
     df_wind['Date'] = pd.to_datetime(df_wind['Date'], format='%Y%m%d')
@@ -160,55 +215,62 @@ def wind_analysis(time, start_date='2022-02-02', end_date='2022-03-23'):
 
     # TODO: Might make sense to group by interval of 12 hours,
     #  currently you're looking at forecast at 0 for 12 and price at 0, get pct change gonna need more params
-    df = df_wind.merge(get_pct_change_dataframe(start_date, end_date), on='trd_delivery_time_start')
+    # df = df_wind.merge(get_pct_change_dataframe(start_date, end_date), on='trd_delivery_time_start')
+    df = df_wind.merge(get_diff(absolute=False, start_date=start_date, end_date=end_date), on='trd_delivery_time_start')
     df.dropna(inplace=True)
     pd.set_option('display.expand_frame_repr', False)
 
+    # TODO: make this return instead
     print(f"Correlation between forecast wind data at hour {time} for 12 hours ahead and price change:")
-    print(df[df.columns[1:14]].apply(lambda x: x.corr(df['percentage_change'], method='pearson')), end=3 * '\n')
+    print(df[df.columns[1:14]].apply(lambda x: x.corr(df['price_diff'], method='pearson')), end=3 * '\n')
 
 
-def stats_plots():
+def plot_seasonality():
+    """
+    Plots observed, trend, seasonal and residual time series
+    """
     df_intra_day = get_intra_day_min_max_mean(on='trd_delivery_time_start', interval='H')
     df_intra_day['trd_delivery_time_start'] = pd.to_datetime(df_intra_day['trd_delivery_time_start'])
     df_intra_day = df_intra_day.set_index('trd_delivery_time_start')
 
-    # Hourly trend, seasonality and residuals
-    result = seasonal_decompose(df_intra_day['trd_price_mean'], period=1)
-    fig = result.plot()
-    ax = fig.gca()
-    ax.xaxis_date()
-    fig.autofmt_xdate()
-    plt.show()
+    def plot(result, title):
+        fig = result.plot()
+        ax = fig.gca()
+        ax.xaxis_date()
+        fig.autofmt_xdate()
+        plt.title(title)
+        plt.tight_layout()
+        plt.savefig(f'{path}/seasonality-{(title.lower())}.png')
 
-    # Daily trend, seasonality and residuals
-    result = seasonal_decompose(df_intra_day['trd_price_mean'], period=24)
-    fig = result.plot()
-    ax = fig.gca()
-    ax.xaxis_date()
-    fig.autofmt_xdate()
-    plt.show()
+    plot(seasonal_decompose(df_intra_day['trd_price_mean'], period=1), 'Hourly')
+    plot(seasonal_decompose(df_intra_day['trd_price_mean'], period=24), 'Daily')
+    plot(seasonal_decompose(df_intra_day['trd_price_mean'], period=168), 'Weekly')
+    plot(seasonal_decompose(df_intra_day['trd_price_mean'], period=730), 'Monthly')
 
-    # Weekly trend, seasonality and residuals
-    result = seasonal_decompose(df_intra_day['trd_price_mean'], period=168)
-    fig = result.plot()
-    ax = fig.gca()
-    ax.xaxis_date()
-    fig.autofmt_xdate()
-    plt.show()
 
-    # TODO: # Monthly trend, seasonality and residuals
+def plot_auto_correlation():
+    """
+    Plots auto-correlation and partial auto-correlation
+    """
+    # Auto-correlation plot
+    df_intra_day = get_intra_day_min_max_mean(on='trd_delivery_time_start', interval='H')
+    df_intra_day['trd_delivery_time_start'] = pd.to_datetime(df_intra_day['trd_delivery_time_start'])
+    df_intra_day = df_intra_day.set_index('trd_delivery_time_start')
+    plot_acf(df_intra_day['trd_price_mean'])
+    plt.savefig(f'{path}/auto_correlation.png')
 
-    # Autocorrelation plot
-    ax = plot_acf(df_intra_day['trd_price_mean'])
-    plt.show()
-
-    # Partial autocorrelation plot
+    # Partial auto-correlation plot
     plot_pacf(df_intra_day['trd_price_mean'], method='ywm')
-    plt.show()
+    plt.savefig(f'{path}/partial_auto_correlation.png')
 
 
-def plot_diff_and_abs_diff(start_date='2022-02-02', end_date='2022-03-24'):
+def plot_diff(start_date='2021-11-09',
+              end_date='2022-03-23'):
+    """
+    Plots density plots and histograms for price differences between intra-day and day-ahead data
+    :param start_date: filter trades to those that happened on or after start_date
+    :param end_date: filter trades to those that happened on or before end_date
+    """
     def plot_df(df):
         df.plot(kind='hist', bins=100, color='blue', edgecolor='black')
         plt.show()
@@ -220,11 +282,15 @@ def plot_diff_and_abs_diff(start_date='2022-02-02', end_date='2022-03-24'):
                      kde_kws={'linewidth': 2})
         plt.show()
 
-    plot_df(get_diff(start_date=start_date, end_date=end_date))
-    plot_df(get_diff(absolute=False, start_date=start_date, end_date=end_date))
+    #plot_df(get_diff(absolute=False, start_date=start_date, end_date=end_date, time_before_closing=30, unit='minutes'))
+    plot_df(get_diff(absolute=False, start_date=start_date, end_date=end_date, interval='H'))
 
 
-def var_and_std_of_diff_by_day():
+def plot_std_of_diff_by_day():
+    """
+    Plots standard deviation between intra-day and day-ahead prices
+    TODO: currently it's for all products in a day, add option to pass max/min time before closing
+    """
     def plot_df(df):
         df = df.groupby(df.index.day_name())['price_diff'].agg(['var', 'std'])
         df.rename(index={'trd_delivery_time_start': 'hour'})
@@ -234,24 +300,33 @@ def var_and_std_of_diff_by_day():
         df = df.set_index('Day').reindex(cats)
         df['std'].plot(kind="bar")
         plt.tight_layout()
-        plt.show()
+        plt.savefig(f'{path}/std_of_diff_by_day.png')
 
     plot_df(get_diff())
-    plot_df(get_diff(absolute=False))
 
 
-def granger_causality(start_date='2022-02-02', end_date='2022-03-24'):
+def granger_causality(start_date='2021-11-09',
+                      end_date='2022-03-23'):
+    """
+    Checks if intra-day price the day before gra
+    :param start_date: filter trades to those that happened on or after start_date
+    :param end_date: filter trades to those that happened on or before end_date
+    :return: pandas dataframe with p-values for granger causality for all hour combinations
+    """
     df = get_intra_day_by_hours(start_date=start_date, end_date=end_date)
     stationary = False
     max_diff = 0
+    # Make time series stationary
     while not stationary:
         stationary = True
         for hour in range(24):
+            # Augmented Dickey-Fuller test
             diff = pmd.arima.ndiffs(df[hour], test="adf")
             if diff > 0:
                 stationary = False
             if diff > max_diff:
                 max_diff = diff
+            # Kwiatkowski–Phillips–Schmidt–Shin test
             diff = pmd.arima.ndiffs(df[hour], test="kpss")
             if diff > 0:
                 stationary = False
@@ -264,33 +339,16 @@ def granger_causality(start_date='2022-02-02', end_date='2022-03-24'):
     return get_grangers_causation_matrix(df, variables=df.columns)
 
 
-def adf_pmd(x):
-    adf_test = pmd.arima.stationarity.ADFTest(alpha=0.05)
-    res = adf_test.should_diff(x)
-    conclusion = "non-stationary" if res[0] > 0.05 else "stationary"
-    resdict = {"should we difference? ": res[1], "p-value ": res[0], "conclusion": conclusion}
-    n_adf = pmd.arima.ndiffs(x, test="adf")
-    return resdict, n_adf
-
-
-def kpss_pmd(x):
-    kpss_test = pmd.arima.stationarity.KPSSTest(alpha=0.05)
-    res = kpss_test.should_diff(x)
-    conclusion = "not stationary" if res[0] <= 0.05 else "stationary"
-    resdict = {"should we difference? ":res[1], "p-value ":res[0], "conclusion":conclusion}
-    n_kpss = pmd.arima.ndiffs(x, test="kpss")
-    return resdict, n_kpss
-
-
 # Taken from
 # https://stackoverflow.com/questions/58005681/is-it-possible-to-run-a-vector-autoregression-analysis-on-a-large-gdp-data-with
-# Might make sense to change maxlag to 24 when more data will be available
-def get_grangers_causation_matrix(data, variables, test='ssr_chi2test', verbose=False, maxlag=13):
+# Max lag is 1 as it was only necessary to difference once
+def get_grangers_causation_matrix(data, variables, test='ssr_chi2test', verbose=False, max_lag=1):
     df = pd.DataFrame(np.zeros((len(variables), len(variables))), columns=variables, index=variables)
+    print(data)
     for c in df.columns:
         for r in df.index:
-            test_result = grangercausalitytests(data[[r, c]], maxlag=maxlag, verbose=False)
-            p_values = [round(test_result[i + 1][0][test][1], 4) for i in range(maxlag)]
+            test_result = grangercausalitytests(data[[r, c]], maxlag=max_lag, verbose=False)
+            p_values = [round(test_result[i + 1][0][test][1], 4) for i in range(max_lag)]
             if verbose: print(f'Y = {r}, X = {c}, P Values = {p_values}')
             min_p_value = np.min(p_values)
             df.loc[r, c] = min_p_value
@@ -302,14 +360,15 @@ def get_grangers_causation_matrix(data, variables, test='ssr_chi2test', verbose=
 if __name__ == "__main__":
     plot_price_at_given_time()
     plot_average_price_of_product()
-    plot_average_price_of_product(time_before_closing=30, unit='minutes')
-    plot_average_price_of_product(time_before_closing=1, unit='hours')
-    day_ahead_as_intra_day_prediction_accuracy()
-    increase_decrease_analysis()
-    wind_analysis(time=0)
-    wind_analysis(time=12)
-    stats_plots()
-    plot_diff_and_abs_diff()
-    var_and_std_of_diff_by_day()
+    plot_average_price_of_product(max_time_before_closing=30, unit='minutes')
+    plot_average_price_of_product(max_time_before_closing=1, unit='hours')
+    get_day_ahead_as_intra_day_prediction_accuracy()
+    get_increase_decrease()
+    get_wind_correlation(time=0)
+    get_wind_correlation(time=12)
+    plot_seasonality()
+    plot_diff()
+    plot_std_of_diff_by_day()
     granger_causation_matrix = granger_causality()
     print(granger_causation_matrix)
+
